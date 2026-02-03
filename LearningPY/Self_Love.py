51607e6717cbcb566518268ad76988fcf0defcd0
James <James@DESKTOP-6AB28FN>
2026-02-03 21:04:51 +0000

    notes updated

--- LearningPY/Self_Love.py
+++ LearningPY/Self_Love.py
@@ -499,3 +499,16 @@ except:
     print("Something went wrong")
 finally:
     print("The 'try except' is finished")
+
+# Try to open and write to a file that is not writable:
+
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
